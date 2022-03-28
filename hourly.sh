261983506db9c1fd985c73b80a260b97ef067eac
Mark Tartagashev <mangoozt@gmail.com>
2022-03-28 22:18:01 +0300

    fix webmap paths in cron scripts

--- hourly.sh
+++ hourly.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "${0%/*}"
 
-docker run --env-file .env -v `pwd`/data/server:/mcdata -v `pwd`/data/backups:/backups -v `pwd`/webmap:/webmap --rm -d my-backup noo
+docker run --env-file .env -v `pwd`/data/server:/mcdata -v `pwd`/data/backups:/backups -v `pwd`/data/webmap:/webmap --rm -d my-backup noo
