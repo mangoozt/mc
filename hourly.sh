9f7a466dc8773071ae3dac000232b222c806420a
Mark Tartagashev <mangoozt@gmail.com>
2022-04-20 16:27:24 +0300

    Update hourly.sh

--- hourly.sh
+++ hourly.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "${0%/*}"
 
-docker run --env-file .env -v `pwd`/data/server:/mcdata -v `pwd`/data/backups:/backups -v `pwd`/data/webmap:/webmap --rm -d my-backup noo
+docker run --env-file .env -v `pwd`/data/server:/mcdata -v `pwd`/data/backups:/backups -v `pwd`/data/webmap:/webmap --rm -d ghcr.io/mangoozt/mc:latest noo
